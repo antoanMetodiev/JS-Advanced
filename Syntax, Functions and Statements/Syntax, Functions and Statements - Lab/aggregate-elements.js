2a2bd2fb0f49e14619d5eb66419099d96934fd22
Antoan Metodiev <Boksnem@abv.bg>
2024-01-29 22:09:03 +0200

    add solution

---
+++ Syntax, Functions and Statements/Syntax, Functions and Statements - Lab/aggregate-elements.js
@@ -0,0 +1,8 @@
+function aggregateElements(array) {
+    let sum = array[0] + array[1] + array[2];
+    console.log(sum);
+    console.log('' + array[0] + array[1] + array[2]);
+}
+
+aggregateElements([1, 2, 3])
+
