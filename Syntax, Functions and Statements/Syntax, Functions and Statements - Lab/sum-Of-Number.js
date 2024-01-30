182f3b41bb3a87d59c2a9fded668ef0732ff52cd
Antoan Metodiev <Boksnem@abv.bg>
2024-01-30 20:23:34 +0200

    add solution

---
+++ Syntax, Functions and Statements/Syntax, Functions and Statements - Lab/sum-Of-Number.js
@@ -0,0 +1,21 @@
+function sumOfNumbers(num1, num2) {
+    let result = 0;
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    for (let i = num1; i <= num2; i++) {
+        result += i;
+    }
+    console.log(result);
+}
+
+
+sumOfNumbers('1', '5'); 
+sumOfNumbers('1', '5');
+sumOfNumbers('1', '5');
+
+
+
+
+
+
