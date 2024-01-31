f15244d4c0e0fb4407fdd5911d0906e53faf4ad3
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-01-31 20:33:09 +0200

    Update square-of-stars.js

--- Syntax, Functions and Statements/Syntax, Functions and Statements - Lab/square-of-stars.js
+++ Syntax, Functions and Statements/Syntax, Functions and Statements - Lab/square-of-stars.js
@@ -1,5 +1,5 @@
-function squareOfStars(n) {
-    n = Number(n);
+function squareOfStars(num) {
+    let n = Number(num);
     for (let i = 1; i <= n; i++) {
         for (let j = 1; j <= n; j++) {
             process.stdout.write('* ')
