6eb8f90343836df49523bdf15f9bd574741cfca4
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-01-31 20:35:20 +0200

    Update math-Operations.js

--- Syntax, Functions and Statements/Syntax, Functions and Statements - Lab/math-Operations.js
+++ Syntax, Functions and Statements/Syntax, Functions and Statements - Lab/math-Operations.js
@@ -1,5 +1,5 @@
 function mathOperations(num1, num2, operator) {
-    let result;
+    let result = 0;
     switch (operator) {
         case '+': result = num1 + num2; break;
         case '-': result = num1 - num2; break;
