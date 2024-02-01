4a61d7a054e1d9dd94e86a140b5f529a4d55b482
Antoan Metodiev <Boksnem@abv.bg>
2024-02-01 22:36:31 +0200

    add solution

---
+++ Syntax, Functions and Statements/Syntax, Functions and Statements - Lab/largest-number.js
@@ -0,0 +1,11 @@
+
+// function largestNumber(num1, num2, num3) {
+//     console.log('The largest number is ' + Math.max(num1, num2, num3) + '.');
+// }
+
+// largestNumber(5, -3, 16)
+
+
+console.log(Boolean('Pesho'));
+console.log(!!'Pesho');
+console.log(Boolean());
