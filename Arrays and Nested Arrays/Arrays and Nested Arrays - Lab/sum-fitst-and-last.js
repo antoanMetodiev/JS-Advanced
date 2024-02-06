474966a180180df508b015fd0a0866f4fff08ebf
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-02-06 19:48:23 +0200

    Update sum-fitst-and-last.js

--- Arrays and Nested Arrays/Arrays and Nested Arrays - Lab/sum-fitst-and-last.js
+++ Arrays and Nested Arrays/Arrays and Nested Arrays - Lab/sum-fitst-and-last.js
@@ -1,10 +1,7 @@
 function sumFirstAndLast(arr) {
-    
     let firstNumber = Number(arr.shift());
     let lastNumber = Number(arr.pop());
-
     console.log(firstNumber +lastNumber);
-
 }
 
 sumFirstAndLast(['5', '10']);
