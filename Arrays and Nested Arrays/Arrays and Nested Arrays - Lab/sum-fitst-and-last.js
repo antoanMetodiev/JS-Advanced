11bde9dbe85de9c5d0dc094d77e1b64f310cbe61
Antoan Metodiev <Boksnem@abv.bg>
2024-02-06 19:47:48 +0200

    add solution

---
+++ Arrays and Nested Arrays/Arrays and Nested Arrays - Lab/sum-fitst-and-last.js
@@ -0,0 +1,10 @@
+function sumFirstAndLast(arr) {
+    
+    let firstNumber = Number(arr.shift());
+    let lastNumber = Number(arr.pop());
+
+    console.log(firstNumber +lastNumber);
+
+}
+
+sumFirstAndLast(['5', '10']);
