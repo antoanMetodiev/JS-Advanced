3db4b7e03750dc4b3251246f833ada678f55600b
Antoan Metodiev <Boksnem@abv.bg>
2024-02-09 13:53:03 +0200

    add solution

---
+++ Arrays and Nested Arrays/Arrays and Nested Arrays - Lab/еqual-Neighbors.js
@@ -0,0 +1,48 @@
+function solve(elements) {
+    let equalElsCount = 0;
+
+    let move = 0;
+    for (let i = 0; i < elements.length; i++) {
+        for (let j = 0; j < elements[i].length; j++) {
+
+            // // for Up:
+            // move = i - 1;
+            // if (move >= 0 && move < elements.length) {
+            //     if (elements[i][j] == elements[i - 1][j]) {
+            //         equalElsCount++;
+            //     }
+            // }
+
+            // for Down:
+            move = i + 1;
+            if (move >= 0 && move < elements.length) {
+                if (elements[i][j] == elements[i + 1][j]) {
+                    equalElsCount++;
+                }
+            }
+
+            // // for Left:
+            // move = j - 1;
+            // if (move >= 0 && move < elements[i].length) {
+            //     if (elements[i][j] == elements[i][j - 1]) {
+            //         equalElsCount++;
+            //     }
+            // }
+
+            // for Right:
+            move = j + 1;
+            if (move >= 0 && move < elements[i].length) {
+                if (elements[i][j] == elements[i][j + 1]) {
+                    equalElsCount++;
+                }
+            }
+        }
+    }
+
+    console.log(equalElsCount);
+}
+
+solve([['test', 'yes', 'yo', 'ho'],
+['well', 'done', 'yo', '6'],
+['not', 'done', 'yet', '5']]);
+
