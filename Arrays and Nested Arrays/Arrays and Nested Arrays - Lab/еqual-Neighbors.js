4a936eb7c2bde7048e131dfbcf07a9094eef867b
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-02-09 13:55:15 +0200

    Update еqual-Neighbors.js

--- Arrays and Nested Arrays/Arrays and Nested Arrays - Lab/еqual-Neighbors.js
+++ Arrays and Nested Arrays/Arrays and Nested Arrays - Lab/еqual-Neighbors.js
@@ -1,17 +1,9 @@
 function solve(elements) {
     let equalElsCount = 0;
-
+    
     let move = 0;
     for (let i = 0; i < elements.length; i++) {
         for (let j = 0; j < elements[i].length; j++) {
-
-            // // for Up:
-            // move = i - 1;
-            // if (move >= 0 && move < elements.length) {
-            //     if (elements[i][j] == elements[i - 1][j]) {
-            //         equalElsCount++;
-            //     }
-            // }
 
             // for Down:
             move = i + 1;
@@ -20,15 +12,7 @@ function solve(elements) {
                     equalElsCount++;
                 }
             }
-
-            // // for Left:
-            // move = j - 1;
-            // if (move >= 0 && move < elements[i].length) {
-            //     if (elements[i][j] == elements[i][j - 1]) {
-            //         equalElsCount++;
-            //     }
-            // }
-
+            
             // for Right:
             move = j + 1;
             if (move >= 0 && move < elements[i].length) {
@@ -38,11 +22,9 @@ function solve(elements) {
             }
         }
     }
-
     console.log(equalElsCount);
 }
 
 solve([['test', 'yes', 'yo', 'ho'],
 ['well', 'done', 'yo', '6'],
 ['not', 'done', 'yet', '5']]);
-
