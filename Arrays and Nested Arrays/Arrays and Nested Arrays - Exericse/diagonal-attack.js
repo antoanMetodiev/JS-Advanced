65f6ab33b6ccdbafbf44bae94d903c32b445441c
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-02-10 21:21:47 +0200

    Update diagonal-attack.js

--- Arrays and Nested Arrays/Arrays and Nested Arrays - Exericse/diagonal-attack.js
+++ Arrays and Nested Arrays/Arrays and Nested Arrays - Exericse/diagonal-attack.js
@@ -1,19 +1,13 @@
 function solve(arg) {
-
     let numbers = [];
     for (let i = 0; i < arg.length; i++) {
-        numbers.push([]); // Добавяне на нов празен ред към матрицата
+        numbers.push([]);
         let els = arg[i].split(' ');
         for (let j = 0; j < els.length; j++) {
             let el = Number(els[j]);
             numbers[i].push(el);
         }
     }
-
-
-    // purvo da si vzema sumite na dvata diagonala!
-    // sled tova da vidq dali sa ravni i ako ne sa prosto da vurna matricata !
-    // ako sa da setvam vseki edin red i colona samo kogato - (i) && (j) ne sa ravni!
 
     // find first Diagonal:
     let firstDiagSum = 0;
@@ -30,26 +24,16 @@ function solve(arg) {
     if (firstDiagSum != secondDiagSum) {
         return numbers.join('\n');
     }
-
-
-
+    
     for (let i = 0; i < numbers.length; i++) {
-
         for (let j = 0; j < numbers[i].length; j++) {
-
             if (i !== j && j != (numbers.length - 1 - i)) {
                 numbers[i].splice(j, 1, secondDiagSum);
             }
         }
-
     }
-
     numbers.forEach(e => console.log(e.join(' ')));
-
-
-
 }
-
 
 console.log(solve(['5 3 12 3 1',
     '11 4 23 2 5',
