c7b301ed94e1e4a34eba1abf4df5fe3f899671e9
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-02-12 20:35:50 +0200

    Update town-population.js

--- Objects & Composition/Objects & Composition - Lab/town-population.js
+++ Objects & Composition/Objects & Composition - Lab/town-population.js
@@ -1,20 +1,16 @@
 function solve(input) {
-
     let towns = {};
     for (const town of input) {
 
         let [key, population] = town.split(' <-> ');
         let currentPopulation = Number(population);
-
         if (towns[key]) {
             let previousPopulation = towns[key];
             towns[key] = previousPopulation + currentPopulation;
-
         } else {
             towns[key] = currentPopulation;
         }
     }
-    
     Object.keys(towns).forEach(e => console.log(`${e} : ${towns[e]}`));
 }
 
