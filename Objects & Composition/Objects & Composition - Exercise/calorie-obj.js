4b445684b5da40a9de685009dbf51356b19a57aa
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-02-13 21:51:31 +0200

    Update calorie-obj.js

--- Objects & Composition/Objects & Composition - Exercise/calorie-obj.js
+++ Objects & Composition/Objects & Composition - Exercise/calorie-obj.js
@@ -1,12 +1,9 @@
 function solve(input) {
-
     let products = {};
-
     for (let i = 0; i < input.length; i += 2) {
 
         let productName = input[i];
         let calories = Number(input[i + 1]);
-
         products[productName] = calories;
     }
     console.log(products);
