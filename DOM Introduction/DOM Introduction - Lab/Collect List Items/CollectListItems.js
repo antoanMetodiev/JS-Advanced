57654ac6b03683aec2ec1170ee623ce968751bba
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-02-23 23:17:23 +0200

    Update CollectListItems.js

--- DOM Introduction/DOM Introduction - Lab/Collect List Items/CollectListItems.js
+++ DOM Introduction/DOM Introduction - Lab/Collect List Items/CollectListItems.js
@@ -1,10 +1,6 @@
 function extractText() {
-    // TODO
-
     let ulElement = document.getElementById('items');
-
     let textareaElement = document.getElementById('result');
 
     textareaElement.textContent = ulElement.textContent;
-
 }
