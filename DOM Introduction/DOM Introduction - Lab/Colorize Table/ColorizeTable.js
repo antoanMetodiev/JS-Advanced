7bfac2cbecb9786dac5b9b767b5f174275492222
Antoan Metodiev <Boksnem@abv.bg>
2024-02-24 21:03:00 +0200

    add solution

---
+++ DOM Introduction/DOM Introduction - Lab/Colorize Table/ColorizeTable.js
@@ -0,0 +1,10 @@
+function colorize() {
+
+    let trElements = document.getElementsByTagName('td');
+
+    for (let i = 0; i < trElements.length; i += 4) {
+
+        trElements[i].style.backgroundColor = 'Teal';
+        trElements[i + 1].style.backgroundColor = 'Teal';
+    }
+}
