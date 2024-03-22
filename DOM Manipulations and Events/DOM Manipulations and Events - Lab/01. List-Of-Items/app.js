2e5f5cc96b9177d46c16314cfddcea774e9f36b2
Antoan Metodiev <Boksnem@abv.bg>
2024-03-22 20:07:25 +0200

    add solution

---
+++ DOM Manipulations and Events/DOM Manipulations and Events - Lab/01. List-Of-Items/app.js
@@ -0,0 +1,7 @@
+function addItem() {
+    let inputText = document.getElementById('newItemText');
+    let ulElement = document.querySelector('ul');
+
+    ulElement.appendChild(document.createElement('li')).textContent = inputText.value;
+    inputText.value = '';
+}
