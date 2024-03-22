1feb761c63a1d280b74b2ec64526418493006cbf
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-03-22 20:14:03 +0200

    Update app.js

--- DOM Manipulations and Events/DOM Manipulations and Events - Lab/03. Add-Delete/app.js
+++ DOM Manipulations and Events/DOM Manipulations and Events - Lab/03. Add-Delete/app.js
@@ -1,22 +1,15 @@
 function addItem() {
     let inputElement = document.getElementById('newItemText');
     let ulElement = document.querySelector('ul');
-
     let liElement = document.createElement('li');
     liElement.textContent = inputElement.value;
 
     let aElement = document.createElement('a');
     aElement.textContent = '[Delete]'
     aElement.href = "#";
-
     aElement.addEventListener('click', onClick);
-        
-    function onClick(event) {
-        console.log(event);
-    }
-
+    
     liElement.appendChild(aElement);
-
     ulElement.appendChild(liElement);
     inputElement.value = '';
 }
