15f9b83601c1fd5bb1042ffc4fccf2e2b4e8b743
Antoan Metodiev <Boksnem@abv.bg>
2024-03-23 15:20:16 +0200

    add solution

---
+++ DOM Manipulations and Events/DOM Manipulations and Events - Exercise/01. Sections/script.js
@@ -0,0 +1,16 @@
+function create(sections) {
+   let parentElement = document.getElementById('content');
+   
+   sections.forEach(sectionText => {
+      let div = document.createElement('div');
+      let pElement = document.createElement('p');
+      pElement.textContent = sectionText;
+
+      pElement.style.display = 'none';
+      div.appendChild(pElement);
+      div.addEventListener('click', (event) => {
+         event.target.querySelector('p').style.display = 'block'
+      });
+      parentElement.appendChild(div);
+   });
+}
