32bcdcdf67d5d3b734357359faf52f3f14816cde
Antoan Metodiev <Boksnem@abv.bg>
2024-03-31 20:23:21 +0300

    add solution

---
+++ DOM Manipulations and Events/DOM Manipulations and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -0,0 +1,27 @@
+function encodeAndDecodeMessages() {
+    let initialText = '';
+    let sendText = '';
+
+    // second textArrea
+    let codeTextArea = document.getElementsByTagName('textarea')[0];
+    let decodeTextArea = document.getElementsByTagName('textarea')[1];
+    codeTextArea.value = '';
+    
+    document.getElementsByTagName('button')[0].addEventListener('click', (event) => {
+        initialText = codeTextArea.value;
+        decodeTextArea.value = '';
+
+        for (let letter of codeTextArea.value) {
+            let num = letter.charCodeAt(0) + 1;
+            sendText += String.fromCharCode(num);
+        }
+        decodeTextArea.value = sendText;
+        sendText = '';
+        codeTextArea.value = '';
+    });
+
+    document.getElementsByTagName('button')[1].addEventListener('click', (event) => {
+        decodeTextArea.value = initialText;
+        initialText = '';
+    });
+}
