32d04cb6a6828c8d3eab3abad83f1cbd33d93197
Antoan Metodiev <Boksnem@abv.bg>
2024-03-31 20:19:25 +0300

    add solution

---
+++ DOM Manipulations and Events/DOM Manipulations and Events - Exercise/02. Time-Converter/solution.js
@@ -0,0 +1,29 @@
+function attachEventsListeners() {
+    let divElements = Array.from(document.getElementsByTagName('div'));
+    
+    divElements.forEach(div => {
+        let inputButton = div.querySelectorAll('input')[1];
+        let labelName = div.querySelector('label').textContent.trim();
+        let inputText = div.querySelector('input');
+
+        inputButton.addEventListener('click', () => {
+            let daysInput = document.getElementById('days');
+            let hoursInput = document.getElementById('hours');
+            let minutesInput = document.getElementById('minutes');
+            let secondsInput = document.getElementById('seconds');
+            daysInput.value = Number(inputText.value);
+
+            if (labelName === 'Hours:') {
+                daysInput.value = Number(inputText.value) / 24
+            } else if (labelName === 'Minutes:') {
+                daysInput.value = Number(inputText.value) / 1440;
+            } else if (labelName === 'Seconds:') {
+                daysInput.value = Number(inputText.value) / 86400;
+            }
+
+            hoursInput.value = daysInput.value * 24;
+            minutesInput.value = hoursInput.value * 60;
+            secondsInput.value = daysInput.value * 86400;
+        });
+    });
+}
