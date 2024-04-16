443415a53f3df058f71566b0968f05e156a82678
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2024-04-16 21:01:15 +0300

    Update dropdown.js

--- DOM Manipulations and Events/DOM Manipulations and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ DOM Manipulations and Events/DOM Manipulations and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,7 +1,6 @@
 function addItem() {
     let textInput = document.getElementById('newItemText');
     let valueInput = document.getElementById('newItemValue');
-
     let optionElement = document.createElement('option');
     optionElement.textContent = textInput.value;
     optionElement.value = valueInput.value;
